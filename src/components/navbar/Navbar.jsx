2497f4119d05f2ade9b017233fdbacc0f7d260de
Imac 4 <imac4@Imacs-iMac.local>
2023-01-30 23:03:00 +0000

    Fix: Added menu links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,26 +3,26 @@ import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
 
-const Menu = () => {
-  <>
-    {" "}
-    <p>
-      <a href="#home">Home</a>
-    </p>
-    <p>
-      <a href="#wgpt3">What is GPT3?</a>
-    </p>
-    <p>
-      <a href="#possibility">Open AI</a>
-    </p>
-    <p>
-      <a href="#features">case Studies</a>
-    </p>
-    <p>
-      <a href="#blog">Library</a>
-    </p>
-  </>;
-};
+// const Menu = () => {
+//   <>
+//     {" "}
+//     <p>
+//       <a href="#home">Home</a>
+//     </p>
+//     <p>
+//       <a href="#wgpt3">What is GPT3?</a>
+//     </p>
+//     <p>
+//       <a href="#possibility">Open AI</a>
+//     </p>
+//     <p>
+//       <a href="#features">case Studies</a>
+//     </p>
+//     <p>
+//       <a href="#blog">Library</a>
+//     </p>
+//   </>;
+// };
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -34,7 +34,21 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <Menu />
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -60,8 +74,21 @@ const Navbar = () => {
         {toggleMenu && (
           <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
-              {" "}
-              <Menu />
+              <p>
+                <a href="#home">Home</a>
+              </p>
+              <p>
+                <a href="#wgpt3">What is GPT3?</a>
+              </p>
+              <p>
+                <a href="#possibility">Open AI</a>
+              </p>
+              <p>
+                <a href="#features">case Studies</a>
+              </p>
+              <p>
+                <a href="#blog">Library</a>
+              </p>
             </div>
             <div className="gpt3__navbar-menu_container-links-sign">
               <p>Sign in</p>
