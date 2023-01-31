38d43d0021401be0fa781b2020379aa182f38b0f
Imac 4 <imac4@Imacs-iMac.local>
2023-01-31 10:34:27 +0000

    Added Brand contant to Brand.jsx

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand  section__padding">
+      <div>
+        <img src={google} alt="Google logo" />
+      </div>
+      <div>
+        <img src={slack} alt="Slack logo" />
+      </div>
+      <div>
+        <img src={google} alt="Atlassian logo" />
+      </div>
+      <div>
+        <img src={dropbox} alt="Dropbox logo" />
+      </div>
+      <div>
+        <img src={shopify} alt="Shopify logo" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
