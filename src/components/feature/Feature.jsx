64c0b1433692631626978a6e5d0dcd4beb66fd23
Imac 4 <imac4@Imacs-iMac.local>
2023-02-04 18:37:07 +0000

    Added features

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -5,7 +5,7 @@ const Feature = ({ title, text }) => {
   return (
     <div className="gpt3__features-container__feature">
       <div className="gpt3__features-container__feature-title">
-        <div></div>
+        <div />
         <h1>{title}</h1>
       </div>
       <div className="gpt3__features-container_feature-text">
